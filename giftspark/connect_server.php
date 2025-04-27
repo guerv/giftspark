e6d459777db39825b4410969793b0cc931097095
Natalia <151586579+guerv@users.noreply.github.com>
2025-04-27 11:51:58 -0400

    Update connect_server.php

--- giftspark/connect_server.php
+++ giftspark/connect_server.php
@@ -1,14 +1,1 @@
-<?php
-/**
- * Connects to database on the server for given user name.
- * 
- */
-try {
-    $dbh = new PDO(
-        "mysql:host=localhost;dbname=mhaskarp_db",
-        "mhaskarp_local",
-        "o-+K(I*1"
-    );
-} catch (Exception $e) {
-    die("ERROR: Couldn't connect. {$e->getMessage()}");
-}
+
