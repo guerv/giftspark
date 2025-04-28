8fbf9c19b8d08e38862d6f2c4658a1b5f38bb55f
PMGithubb <110183473+PMGithubb@users.noreply.github.com>
2025-04-27 21:49:59 -0400

    Delete giftspark directory

--- giftspark/data/user_sessions.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user_sessions (
-    session_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNSIGNED NOT NULL,  -- Ensure user_id is the same type as in user_info
-    login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES user_info(user_id) ON DELETE CASCADE
-) 
